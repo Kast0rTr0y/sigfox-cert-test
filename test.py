435a8e2db66b9bdaf89b7f715937a59537a270f5
Peter Putz <peter@pycom.io>
2020-01-03 10:38:06 +0100

    - improved docs - some region 3 changes (config words) still WIP - rgb red=send, green=idle, blue=test_mode - re-enable r2+4 deadlock workaround (needed for latest rc firmware)

--- test.py
+++ test.py
@@ -12,27 +12,28 @@ import machine
 
 ## for use with Sigfox Radio Signal Analyzer
 ## Uplink
-# test = "UL - RF" # ManualVerdict
+# test = "UL - RF" # ManualVerdict, same for Vnom/Vmin/Vmax
 # test = "UL - Protocol" # [2 minutes]
 # test = "UL - Non Volatile Memory" # ManualVerdict
 # test = "UL - Public Key"
-# test = "UL - Frequency Distribution" # [20-30 minutes] (start 11:26) with config 14 and reset until tests 11+12 are complete (config 34 running until the end is more than 1h)
-test = "UL - Frequency Synthesis"  ## [<= 2 minutes]
+test = "UL - Frequency Distribution" # [<= 15 minutes]
+# test = "UL - Frequency Synthesis"  ## [<= 2 minutes]
 ## Downlink:
-test = "DL - Downlink" # Use for any of "DL-Protocol", "DL-Start of Listening" and "DL-End of Listening"
-test = "DL - Link Budget" # DL-Link Budget [439s couple minutes, then test is complete and you can reset the DUT]
-# test = "DL - GFSK Receiver" # Test Mode TX-BPSK has to be executed just before this test.
+#test = "DL - Downlink" # Use for any of "DL-Protocol", "DL-Start of Listening" and "DL-End of Listening"
+# test = "DL - Link Budget" # DL-Link Budget [~ 5 minutes, then test is completed in RSA and you can reset the DUT]
+# test = "DL - GFSK Receiver" # ManualVerdict [30 seconds], Test Mode TX-BPSK has to be executed just before this test.
 
 ## for use with Sigfox Network Emulator
-# test = "MyTest"
+test = "MyTest"
 # test = "MyDownlink"
+
 # test = "None"
 
 
 
 ##############################################################
 # ## select the region
-RCZ = Sigfox.RCZ2
+RCZ = Sigfox.RCZ3
 
 
 # RCZ1
@@ -83,7 +84,11 @@ print("sigfox PAC:", ubinascii.hexlify(sigfox.pac()))
 print("sigfox region:", RCZ)
 print("sigfox frequencies:", sigfox.frequencies())
 print("sigfox info:")
-sigfox.info() # normally prints nothing, but I hacked it to print private key
+sigfox.info() # normally prints nothing, but patched FW prints private key
+#if RCZ == Sigfox.RCZ3:
+#    sigfox.config((0x1, 0x2ee0, 0x100))
+# sigfox.config((3,5000,0))
+print("sigfox config:", sigfox.config()) # after reset RCZ3 defaults to (3, 5000, 0)
 
 #sigfox.public_key(True)
 print("sigfox public key", sigfox.public_key())
@@ -109,6 +114,9 @@ print("test:", test)
 
 wait = 24 # how many seconds do we need to wait after every second frame (starting after the first) in RCZ2 and 4
 last = -wait # when was the last sigfox message sent
+rgb_send = 0x110000
+rgb_idle = 0x000a00
+rgb_test = 0x000011
 
 def sleep(s):
     print("sleep", s, end="")
@@ -123,17 +131,41 @@ def send(msg):
     global last
     global wait
     global s
-    # if RCZ == Sigfox.RCZ2 or RCZ == Sigfox.RCZ4:
-    #     delay = utime.time() - last
-    #     if delay < wait:
-    #         sleep(wait-delay)
-    # else:
-    sleep(1)
+    if RCZ == Sigfox.RCZ2 or RCZ == Sigfox.RCZ4:
+        wait_so_far = utime.time() - last
+        if wait_so_far < wait:
+            sleep(wait - wait_so_far)
+    else:
+        pass
+        sleep(1)
     print("send", ubinascii.hexlify(msg))
+    pycom.rgbled(rgb_send)
     last = utime.time()
     r = s.send(msg)
+    pycom.rgbled(rgb_idle)
     return r
 
+def send_bit(b):
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_BIT, b)
+    pycom.rgbled(rgb_send)
+    s.send('')
+    pycom.rgbled(rgb_idle)
+    sleep(1)
+
+def send_oob():
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_OOB, True)
+    pycom.rgbled(rgb_send)
+    s.send('')
+    pycom.rgbled(rgb_idle)
+    sleep(1)
+    # disable Out-Of-Band to use the socket normally
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_OOB, False)
+
+
+def test_mode(t,c):
+    pycom.rgbled(rgb_test)
+    sigfox.test_mode(t,c)
+    pycom.rgbled(rgb_idle)
 
 
 
@@ -147,33 +179,22 @@ def send(msg):
 ################################################################################
 # run test
 start = utime.time()
-# pycom.heartbeat(False)
-# pycom.rgbled(0x000300)
+pycom.heartbeat(False)
 if test == "UL - RF":
-    for x in range(0,2):
-        sigfox.test_mode( SFX_TEST_MODE_TX_BPSK, 3)
-        sleep(5)
+    test_mode( SFX_TEST_MODE_TX_BPSK, 1)
 elif test == "UL - Protocol":
     # send a single bit: 0
     print("send False")
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_BIT, False)
-    s.send('')
-    sleep(1)
+    send_bit(False)
 
     # send a single bit: 1
     print("send True")
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_BIT, True)
-    s.send('')
-    sleep(1)
+    send_bit(True)
 
     # send an out of bound msg
     print("send OOB")
     # use the socket to send a Sigfox Out Of Band message
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_OOB, True)
-    s.send('')
-    sleep(1)
-    # disable Out-Of-Band to use the socket normally
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_OOB, False)
+    send_oob()
 
     send(bytes([0x40]))
     send(bytes([0x40, 0x41]))
@@ -190,40 +211,57 @@ elif test == "UL - Protocol":
 # elif test == "TX-PROTOCOL Uplink Encrypted Payload":
 #     print("todo")
 elif test == "UL - Non Volatile Memory":
-    send(bytes([8]))
+    if RCZ == Sigfox.RCZ2 or RCZ == Sigfox.RCZ4:
+        # send one bit
+        send_bit(True)
+    else:
+        # if RCZ == Sigfox.RCZ3:
+        #     sigfox.config((0x1, 0x2ee0, 0x100))
+        # send one byte
+        send(bytes([8]))
 elif test == "UL - Public Key":
     sigfox.public_key(True)
+    # if RCZ == Sigfox.RCZ3:
+    #     sigfox.config((0x1, 0x2ee0, 0x100))
     send(bytes([1]))
     sigfox.public_key(False)
 elif test == "UL - Frequency Distribution":
-    sigfox.test_mode(SFX_TEST_MODE_TX_PROTOCOL, 14)
+    # if RCZ == Sigfox.RCZ3:
+    #     sigfox.config((0x1, 0x2ee0, 0x100))
+    test_mode(SFX_TEST_MODE_TX_PROTOCOL, 14)
     # Loop 0 to (config & 0x7F)
     # bit 7 causes a delay between the frames
 elif test == "UL - Frequency Synthesis":
-    sigfox.test_mode(SFX_TEST_MODE_TX_SYNTH, 0)
+    # if RCZ == Sigfox.RCZ3:
+    #     sigfox.config((0x1, 0x2ee0, 0x100))
+    test_mode(SFX_TEST_MODE_TX_SYNTH, 0)
 
 
 
 
 ################################################################################
 elif test == "DL - Downlink":
-    sigfox.test_mode(SFX_TEST_MODE_RX_PROTOCOL, 1)
+    # if RCZ == Sigfox.RCZ3:
+    #     sigfox.config((0x1, 0x2ee0, 0x100))
+    test_mode(SFX_TEST_MODE_RX_PROTOCOL, 1)
 elif test == "DL - Link Budget":
-    sigfox.test_mode(SFX_TEST_MODE_RX_SENSI, 31) # 14:11
+    # if RCZ == Sigfox.RCZ3:
+    #     sigfox.config((0x1, 0x2ee0, 0x100))
+    test_mode(SFX_TEST_MODE_RX_SENSI, 31) # 14:11
     # Loop 0 to (config x 10),
     # config should be 100 as per test protocol,
     # this takes very long
     # config = 1 takes about 127 seconds
     # config = 5 takes about 624 seconds
-    # it seems after 30 frames (ie config = 3), RSA finished the test with INCONCLUSIVE
+    # it seems after 30 frames (ie config = 3), RSA finished the test
 elif test == "DL - GFSK Receiver":
-    print("Test Mode TX-BPSK has to be executed just before this test.")
+    print("NB: Test Mode TX-BPSK has to be executed just before this test.")
     # print("Run test_mode(BPSK) before GFSK ... ")
     # sigfox.test_mode( SFX_TEST_MODE_TX_BPSK, 3)
     # print("... done. Click on \"Start send GFSK\" in RSA now")
     # sleep(2)
     # print("Run test_mode(GFSK) ...")
-    sigfox.test_mode(SFX_TEST_MODE_RX_GFSK, 30) # 30 seconds timeout
+    test_mode(SFX_TEST_MODE_RX_GFSK, 30) # 30 seconds timeout
     # print("... done.")
     print("rssi", sigfox.rssi())
 
@@ -235,13 +273,17 @@ elif test == "DL - GFSK Receiver":
 
 ################################################################################
 elif test == "MyTest":
-    for b in range(0, 10):
+    # print("a")
+    # #send(bytes([1,2,3,4,5,6,7,8,9,0xa,0xb,0xc]))
+    # send(bytes([7]))
+    # print("b")
+    for b in range(0, 100):
         x = utime.time()
         d = b % ( 0xff + 1 )
         retval = send(bytes([d]))
         print("sent after", utime.time() -x, "seconds")
         print("retval", retval)
-        sleep(2)
+        #sleep(2)
 elif test == "MyDownlink":
     x = utime.time()
     retval = send(bytes([0x0f]))
